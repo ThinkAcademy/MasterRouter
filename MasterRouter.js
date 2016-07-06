b52eac6da4aab30cc6b2b0ce2b5b3b2c9bc2b29f
Think Academy <admin@thinkacademy.io>
2016-07-06 15:58:24 -0400

    Update MasterRouter.js

--- MasterRouter.js
+++ MasterRouter.js
@@ -1,157 +1,189 @@
 // A simple Routing plugin for the MasterControl JAVSCRIPT framework by Alexander Think - MIT Licensed
-// version 1.1 currently on works with HTML 5 browsers using history.js plugin
+// version 1.5 currently on works with HTML 5 browsers using history.js plugin
 //https://github.com/browserstate/history.js/
 
 // TODO: Check to see if user has declaired same controller name and same action name using html so that there is no double call
+(function(window, undefined) {
 
-(function ( window, undefined) {
+    var Router = function(app, ishash) {
 
-    var Router = function(app){
-
+        this.APPmodule = null;
         this.$$routingList = [];
         // check to see if master control is installed
-    	this.init = function () {
+        this.init = function() {
 
-                var APPmodule = document.querySelector("[fan-routing-view]");
+            APPmodule = document.querySelector("[fan-routing-view]");
 
-                if(APPmodule !== null){
+            if (ishash === null) {
+                ishash = false;
+            }
 
-                    var urlArray = window.location.pathname.split("/");
-                    var controller = urlArray[1];
-                    var action = urlArray[2];
+            if (APPmodule !== null) {
 
-                    if(action == '' || action == undefined){
-                        action = "index";
+                if (ishash === true) {
+                    var controller = getController(window.location.hash.split("/"));
+                    var action = getAction(window.location.hash.split("/"));
+                    // if controller is empty then try running non hash check
+                    if (controller === '' || controller === undefined) {
+                        var nonHashController = getController(window.location.pathname.split("/"));
+                        var nonHashAction = getAction(window.location.pathname.split("/"));
+                        digest(nonHashController, nonHashAction);
+
+                    } else {
+                        digest(controller, action);
                     }
 
-                    if($$routingList.length > -1){
+                    // this starts listening to url hash changes
+                    window.onhashchange = function() {
+                        var controller = getController(window.location.hash.split("/"));
+                        var action = getAction(window.location.hash.split("/"));
+                        digest(controller, action);
+                    };
 
-                        var calledController = false;
-                        var calledAction = false;
-
-                        for(var r = 0; $$routingList.length > r; r++ ){
-                                // reset the url if slash or empty same thing
-                                if($$routingList[r].controllerURL === "/"){
-                                    $$routingList[r].controllerURL = "";
-                                }
-
-                                if($$routingList[r].actionURL === ""){
-                                    $$routingList[r].actionURL = "index";
-                                }
-
-                                if($$routingList[r].controllerURL === controller){
-                                        calledController = true;
-                                        // CALL MASTERCONTROLLER CONTROLLER WITH SAME NAME
-                                        var error404MSG = app.callController($$routingList[r].controller, APPmodule);
-                                        // if error message is 404 not found
-                                        if(error404MSG != null){
-                                                  // then call 404Error controller in master controller
-                                                var errorMSG = app.callController("error404", APPmodule);
-                                                if(errorMSG != null){
-                                                    var errorMessage = "Error cannot find controller with name error404. please make an error controller to continue";
-                                                    throw new Error(errorMessage);
-                                                }
-                                        }
-                                        else{
-                                            
-                                            if($$routingList[r].actionURL === action){
-                                                    app.callAction($$routingList[r].action, $$routingList[r].controller, APPmodule);
-                                                    calledAction = true;
-                                            }
-                                        }
-                                }    
-
-                        }
-
-                        if(calledController === false){
-                            // CALL MASTERCONTROLLER CONTROLLER WITH SAME NAME
-                            if(!controller){
-                                var errorMessage = "Error controller is blank or couldn't find route with empty name";
-                                throw new Error(errorMessage);
-                            }
-                            else{
-
-                                var error404MSG = app.callController(controller, APPmodule);
-
-                                if(error404MSG != null){
-                                    // then call 404Error controller in master controller
-                                    var errorMSG = app.callController("error404", APPmodule);
-                                    if(errorMSG != null){
-                                        var errorMessage = "Error cannot find controller with name error404. please make one to continue";
-                                        throw new Error(errorMessage);
-                                    }
-
-                                }
-                                else{
-                                    if(calledAction === false){
-                                        app.callAction(action, controller, APPmodule);
-                                    }
-                                }
-                            }
-                        }
-
-                    }
-
-                    else{
-                            if(!controller){
-                                var errorMessage = "Error controller is blank or couldn't find route with empty name";
-                                throw new Error(errorMessage);
-                            }
-                            else{
-                                                // CALL MASTERCONTROLLER CONTROLLER WITH SAME NAME
-                                var error404MSG = app.callController(controller, APPmodule);
-
-                                if(error404MSG != null){
-                                    // then call 404Error controller in master controller
-                                    var errorMSG = app.callController("error404", APPmodule);
-                                    if(errorMSG != null){
-                                        var errorMessage = "Error cannot find controller with name error404. please make one to continue";
-                                        throw new Error(errorMessage);
-                                    }
-                                }
-                                else{
-                                    app.callAction(action, controller, APPmodule);
-                                }
-                            }
-                    }
+                } else {
+                    var controller = getController(window.location.pathname.split("/"));
+                    var action = getAction(window.location.pathname.split("/"));
+                    digest(controller, action);
                 }
+
+            }
+        };
+
+        this.getController = function(urlArray) {
+            return urlArray[1];
+        };
+
+        this.getAction = function(urlArray) {
+            return urlArray[2];
+        };
+
+        this.digest = function(controller, action) {
+
+            if (action === '' || action === undefined) {
+                action = "index";
             }
 
 
-            return{
-                routeTo: function(controllerURL, actionURL, controller, action){
-                    
-                    var routing = {
-                        controllerURL: controllerURL,
-                        actionURL: actionURL,
-                        controller: controller,
-                        action: action
+            if ($$routingList.length > -1) {
+
+                var calledController = false;
+                var calledAction = false;
+
+                for (var r = 0; $$routingList.length > r; r++) {
+
+                    // reset the url if slash or empty same thing
+                    if ($$routingList[r].controllerURL === "/") {
+                        $$routingList[r].controllerURL = "";
                     }
 
-                    $$routingList.push(routing );
-                    return this
+                    if ($$routingList[r].actionURL === "") {
+                        $$routingList[r].actionURL = "index";
+                    }
 
-                },
+                    if ($$routingList[r].controllerURL === controller) {
+                        calledController = true;
+                        // CALL MASTERCONTROLLER CONTROLLER WITH SAME NAME
+                        var error404MSG = app.callController($$routingList[r].controller, APPmodule);
+                        // should return null
+                        if (error404MSG !== null) {
+                            // then call 404Error controller in master controller
+                            var errorMSG = app.callController("error404", APPmodule);
+                            if (errorMSG !== null) {
+                                var errorMessage = "Error cannot find controller with name error404. please make an error controller to continue";
+                                throw new Error(errorMessage);
+                            }
+                        } else {
 
-                start:function(){
-                    if (typeof app == "object") {
-                            init();
+                            if ($$routingList[r].actionURL === action) {
+                                app.callAction($$routingList[r].action, $$routingList[r].controller, APPmodule);
+                                calledAction = true;
+                            }
                         }
-                        else{
-                            throw new Error("Master Control plugin needs to be installed first");
+                    } else {
+
 
                     }
-                    return this
+
                 }
 
+                if (calledController === false) {
+                    // CALL MASTERCONTROLLER CONTROLLER WITH SAME NAME
+                    if (!controller) {
+                        var errorMessage = "Error controller is blank or couldn't find route with empty name";
+                        throw new Error(errorMessage);
+                    } else {
+
+                        var error404MSG = app.callController(controller, APPmodule);
+                        // should return null if ok
+                        if (error404MSG !== null) {
+                            // then call 404Error controller in master controller
+                            var errorMSG = app.callController("error404", APPmodule);
+                            if (errorMSG !== null) {
+                                throw new Error("Error cannot find controller with name error404. please make one to continue");
+                            }
+
+                        } else {
+                            if (calledAction === false) {
+                                app.callAction(action, controller, APPmodule);
+                            }
+                        }
+                    }
+                }
+
+            } else {
+                if (!controller) {
+                    var errorMessage = "Error controller is blank or couldn't find route with empty name";
+                    throw new Error(errorMessage);
+                } else {
+                    // CALL MASTERCONTROLLER CONTROLLER WITH SAME NAME
+                    var error404MSG = app.callController(controller, APPmodule);
+                    // should return null if ok
+                    if (error404MSG !== null) {
+                        // then call 404Error controller in master controller
+                        var errorMSG = app.callController("error404", APPmodule);
+                        if (errorMSG !== null) {
+                            var errorMessage = "Error cannot find controller with name error404. please make one to continue";
+                            throw new Error(errorMessage);
+                        }
+                    } else {
+                        app.callAction(action, controller, APPmodule);
+                    }
+                }
+            }
+        };
+
+
+        return {
+            routeTo: function(controllerURL, actionURL, controller, action) {
+
+                var routing = {
+                    controllerURL: controllerURL,
+                    actionURL: actionURL,
+                    controller: controller,
+                    action: action
+                };
+
+                $$routingList.push(routing);
+                return this;
+
+            },
+
+            start: function() {
+                if (typeof app == "object") {
+                    init();
+                } else {
+                    throw new Error("Master Control plugin needs to be installed first");
+
+                }
+                return this;
             }
 
         };
 
-        window.MasterRouter = Router;
+    };
+
+    window.MasterRouter = Router;
 })(window);
-
-
 
 
 /********************************************************************************************************************************/
